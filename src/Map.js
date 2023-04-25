7e9bacf9144e2b9c052ec07c3c6399137a7f0022
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2023-04-24 22:16:38 -0400

    First commit

---
+++ src/Map.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// // import { MapView, WebMap } from '@arcgis/core';
+// import { MapView } from '@arcgis/core/views';
+// import WebMap from '@arcgis/core/WebMap';
+
+// class Map extends Component {
+//   componentDidMount() {
+//     const map = new WebMap({
+//       portalItem: {
+//         // replace with the ID of the web map you want to use
+//         id: 'e0f91c999f544574b9cd9fab589e9b3c'
+//       }
+//     });
+
+//     const view = new MapView({
+//       container: 'viewDiv',
+//       map: map
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div id="viewDiv"></div>
+//     );
+//   }
+// }
+
+// export default Map;
+
