d8165f0048d94c52b626a8768d30ce61be282406
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2023-04-25 23:57:59 -0400

    Trying to integrate the OPenAI code.  Not working yet. :(

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import Buttons from './Buttons';
 // import Map from './Map'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
